a9926dc205675be9c9bd9009c1c754a4e3768044
MrRakib404cyber <115956303+MrRakib404cyber@users.noreply.github.com>
2022-11-29 22:36:21 +0300

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,33 +1,8 @@
-import os, platform
- 
-try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from PY6 import RIKI
- 
-        RIKI()
- 
- 
- 
-elif bit == "32bit":
- 
-        from PY3 import RIKI
- 
- 
-        RIKI()
- 
- 
- 
+import os
+print(" Update Checking")
+os.system("git pull")
+
+
+
+import RANDOM_TEST
+RANDOM_TEST.login()
